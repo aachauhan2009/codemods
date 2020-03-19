b1741a398f447769d958872c7fbe5c787016e3ea
Amit Chauhan <a.a.chauhan2009@gmail.com>
2020-03-19 17:15:42 +0530

    fixed issue#1

--- eslint-transform.js
+++ eslint-transform.js
@@ -1,14 +1,9 @@
-/*eslint-disable*/
 module.exports = function(file, api, options) {
     const j = api.jscodeshift;
     const root = j(file.source);
-  
-  
     const emptyState = j.emptyStatement();
     const comment = j.commentBlock(" eslint-disable ", false, true);
     const getFirstNode = () => root.find(j.Program).get("body", 0);
-  
-    // Save the comments attached to the first node
     emptyState.comments = [comment];
     getFirstNode().insertBefore(emptyState);
     return root.toSource();
